994b19cfe4cab1e3df0891d69c225e31e84df189
FEI CAO <122421787+875216663@users.noreply.github.com>
2024-09-04 17:32:08 +0800

    结束到border

--- vue-admin-template-master/src/views/product/tradeMark/index.vue
+++ vue-admin-template-master/src/views/product/tradeMark/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>平台属性管理 
+  <div>
+    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0px">
+      新增
+    </el-button>
+    <!-- 
+         表格组件 
+         data:表格组件将来需要展示的数据------数组类型
+         border：是给表格添加边框
+         column属性
+         label：显示标题
+         width：对应列的宽度
+         align：标题的对齐方式
+         prop:对应列内容的字段名
+         type:如果设置了索引那么就从一开始
+         border：边框
+         注意1：elementUI当中的table组件，展示的数据是以一列一列进行展示数据
+       -->
+    <el-table style="width: 100%" border>
+      <el-table-column prop="prop1" label="Label 1" width="width">
+      </el-table-column>
+      <el-table-column prop="prop2" label="Label 2" width="width">
+      </el-table-column>
+      <el-table-column prop="prop3" label="Label 3" width="width">
+      </el-table-column>
+      <el-table-column prop="prop4" label="Label 4" width="width">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -7,7 +33,7 @@
 export default {
   name: "TradeMark",
 };
-</script>
+</script> 
 
 <style>
 </style> 
